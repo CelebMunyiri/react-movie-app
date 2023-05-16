4a027891228a49b3da03a7575939be553fed3726
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-16 17:42:21 +0300

    updated the index file so it works in react 18

---
+++ index.js
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+//import ReactDOM  from "react-dom";
+//import { createRoot } from 'react-dom/client';
+
+import App from './App'
+
+{/*ReactDOM.render(
+    <App/>,document.getElementById('root')
+)*/}
+createRoot(document.getElementById('root')).render(<App />);
