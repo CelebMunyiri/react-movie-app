6c77a549279b8e4e066799ed759d4af116087604
David Munyiri <davidmumyiri2019@gmail.com>
2023-05-16 10:12:08 +0300

    updated files and folders setup for the project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+//const API_URL=': http://www.omdbapi.com/?i=tt3896198&apikey=51ebdfab'
+const App=()=>{
+    return(
+        <>
+        <h1>ISLAND OF MOVIES</h1>
+        </>
+    )
+}
+export default App
